87ea1095098baf132fc4fcf063a1ba31f5fec2aa
Nikhar Abbas <nikhar.abbas@colorado.edu>
2019-07-31 10:20:54 -0600

    Adding NREL 5MW Run Files

---
+++ SimulinkControllers/TSR_Tracking/NREL_5MW/Pre_ContParam_TSR_NREL5MW.m
@@ -0,0 +1,54 @@
+function param = Pre_ContParam_TSR_NREL5MW
+% Control parameters input file for TSR_Tracking_*.mdl
+%
+% These parameters are specific for the NREL 5MW (fixed bottom) controller
+%
+% Nikhar Abbas - May 2019
+
+% Turbine and Environmental ParametersParameters
+param.J = 38677056.000 + 534.116;           % Total Rotor Inertia, kg-m^2.
+param.RotorRad      = 63;                % Rotor Radius, m.
+param.GBRatio       = 97;                   % Gearbox Ratio, -.
+param.GenEff        = .944;                 % Generator Efficiency, 1/%
+param.RRSpeed       = 12.1 * pi/30;          % Rated Rotor Speed, rad/s.
+param.rho           = 1.225;                % Air Density, kg/m^3.
+
+% Filter Cornering Frequencies
+param.filt_HSS           = 4.519 * 1/2;    % Corner frequency in the recursive, second order low pass filter of the HSS Speed, Hz. -- Chosen to be 3/4 of the first drivetrain natural frequency
+param.filt_WindSpeedEst  = 0.2;            % Corner frequency for Wind Speed Filt, Hz. -- (Currently arbitrary)
+param.filt_GB            = 1/10;           % Corner frequeny for Gain Bias Filter, Hz. -- (Currently arbitrary)
+
+% Variable Speed Torque Controller Parameters
+param.VS_zeta = 1;                        % Damping constant, --
+param.VS_om_n = 0.3; 1/(2*6.25);                 % Natural frequency, Hz. -- Time constant chosen to be on third the rotor frequency at rated. 
+
+% Blade Pitch Controller Parameters
+param.PC_zeta = 0.7;                         % Damping constant, --
+param.PC_om_n = 0.6;                         % Natural frequency, Hz.
+
+% Region 2.5 Gain Bias
+param.VS_GainBias = 60; 0.8; 0.31; 0.031;              % VS Controller Bias Gian for Region 2.5 smoothing, -.
+param.PC_GainBias = .0001 ;0.081; 0.081; 0.81;              % Pitch Controller Bias Gian for Region 2.5 smoothing, -.
+
+% Wind Speed Estimator
+param.WSE_v0    = 11.4;
+
+% Pitch Controller Setpoints
+param.PC_MaxPit     = 90 * (pi/180);        % Maximum pitch setting in pitch controller, rad.
+param.PC_MaxRat     = 10 * (pi/180);        % Maximum pitch  rate (in absolute value) in pitch  controller, rad/s.
+param.PC_MinPit     = -0.8  * (pi/180);        % Minimum pitch setting in pitch controller, rad.
+param.PC_RefSpd     = 122.9096;              % Desired (reference) HSS speed for pitch controller, rad/s.
+param.PC_Vrated     = 11.4; 11.4;                 % Rated wind speed, m/s.
+param.PC_Vmax       = 25;                   % Maximum wind speed, m/s.
+
+% Variable Speed Torque Controller setpoints
+param.VS_MaxRat     = 15000.0;                                             % Maximum torque rate (in absolute value) in torque controller, N-m/s.
+param.VS_Rgn3MP     = 1 * pi/180;                                          % Minimum pitch angle at which the torque is computed as if we are in region 3 regardless of the generator speed, rad. -- chosen to be 1.0 degree above PC_MinPit
+param.VS_RtGnSp     = 122.9096;                                            % Rated generator speed (HSS side), rad/s. 
+param.VS_RtPwr      = 5000000.0/param.GenEff;                              % Rated generator generator power in Region 3, Watts. -- chosen to be 10MW divided by the electrical generator efficiency of 96%
+param.VS_RatedTq    = param.VS_RtPwr/param.PC_RefSpd;                      % Rated generator torque in Region 3 (HSS side), N-m.
+param.VS_MaxTq      = param.VS_RatedTq * 1;                              % Maximum generator torque in Region 3 (HSS side), N-m.
+param.VS_MinSpd     = 6.9*pi/30;                                           % Minimum rotor speed (rad/s)
+param.VS_Vmin       = 3;                                                   % Minimum wind speed, m/s
+
+end
---
+++ SimulinkControllers/TSR_Tracking/NREL_5MW/Run_NREL_5MW_TSR.m
@@ -0,0 +1,114 @@
+%% TSR Referenece Controller 
+% Run the NREL 5MW Turbine simulation initialized with this script
+%
+% This implements a controller to track tip-speed-ratio in below rated
+% operation, and optimal rotor speed in above rated operation. A formal
+% writeup of the logic used will be presented at WindTech 2019. 
+
+%% Setup Paths
+
+% Fast Model 
+ModDir = '/Users/nabbas/Documents/TurbineModels/NREL_5MW/5MW_Land';
+
+% Controller path
+ContPath = '/Users/nabbas/Documents/TurbineModels/TurbineControllers/SimulinkControllers/TSR_Tracking/';
+addpath(ContPath)
+
+%% Define Filenames
+ModName = '5MW_Land.fst';
+ElastoFile = 'NRELOffshrBsline5MW_Onshore_ElastoDyn.dat';
+ServoFile = 'NRELOffshrBsline5MW_Onshore_ServoDyn.dat';
+
+FAST_InputFileName = [ModDir filesep ModName];
+
+%% Simulation and controller setup
+servoparams = {'PCMode', 'VSContrl', 'HSSBrMode', 'YCMode'};
+Pre_FastMod([ModDir filesep ServoFile], servoparams, {4,4,4,4});
+% Pre_FastMod([ModDir filesep ServoFile], servoparams, {5,5,0,0});
+FastIn = Pre_SimSetup(ModDir, ModName, ElastoFile);
+dt = FastIn.DT;
+TMax = FastIn.TMax;         
+ContParam = Pre_ContParam_TSR_NREL5MW;
+
+%% Load CpSurface 
+cpscan = load('/Users/nabbas/Documents/TurbineModels/NREL_5MW/CpScan/CpScan.mat');
+TSRvec = cpscan.TSR;
+zind = find(cpscan.BlPitch == 0);
+Cpvec = cpscan.Cpmat(:,zind)';
+TSR_opt = TSRvec(Cpvec == max(Cpvec));
+TSR_opt = 7.55
+% % Betavec = cpscan.BlPitch(zind:end);
+% % Cpmat = cpscan.Cpmat(:,zind:end);
+
+%% Find plant parameters
+[Avec,Bbvec,GS,Beta_op,vv] = Pre_TSRtracking_GS(ContParam,cpscan);
+ContParam.GS = GS;
+
+% Trim for BldPitch Controller
+Bopind = find(Beta_op>0);
+Avec_BPC = Avec(Bopind(1):end);
+Bbvec_BPC = Bbvec(Bopind(1):end);
+Betaop_BPC = Beta_op(Bopind(1):end);
+vv_bpc = vv(Bopind(1):end);
+%% Define Filter Parameters
+[Filt.HSS.b,Filt.HSS.a] = filt_2lp_D(ContParam.filt_HSS,1,dt); % ContParam.HSSfilt_omn
+[Filt.GBFilt.b,Filt.GBFilt.a] = filt_1lp_D(ContParam.filt_GB,dt); 
+[Filt.Wind.b,Filt.Wind.a] = filt_1lp_D(ContParam.filt_WindSpeedEst,dt); 
+
+%% Load Outlist
+OutName = '5MW_Land.SFunc.out';
+SFunc_OutfileName = [ModDir filesep OutName];
+OutList = Post_LoadOutlist(SFunc_OutfileName); 
+%% Run Simulation
+% % TSR_opt = TSRvec(Cpvec == max(Cpvec));
+sim('TSR_Tracking_v2.mdl',[0,TMax]);
+
+for i = 1:length(OutList)
+    try
+        simout.(OutList{i}) = FAST_Out.Data(:,i);
+    catch
+        warning(['Outlist Parameter ' OutList{i} ' was not loaded from the fast.out file.'])
+    end
+end
+simout.VSparams_a = VS_params.signals.values(:,1);
+simout.VSparams_rotspeederr = VS_params.signals.values(:,2);
+simout.VSparams_Ki = VS_params.signals.values(:,3);
+simout.VSparams_Kp = VS_params.signals.values(:,4);
+simout.TSR = simout.RotSpeed./simout.Wind1VelX * ContParam.RotorRad * pi/30;
+simout.VSparams_omopt = Om_opt.Data;
+
+simout.PCparams_a = PC_params.signals.values(:,1);
+simout.PCparams_rotspeederr = PC_params.signals.values(:,2);
+simout.PCparams_Ki = PC_params.signals.values(:,3);
+simout.PCparams_Kp = PC_params.signals.values(:,4);
+simout.PCparams_B_ss = PC_params.signals.values(:,5);
+
+simout.vhat = vhat.Data;
+simout.ContParam = ContParam;
+%% Plots, if you want them
+% Pl_FastPlots(simout);
+
+% Setpoint smoothing
+delplots = 1;
+if delplots
+    figure(1)
+    myplot(DelOmega); hold on
+    title('DelOmega')
+    figure(2)
+    myplot(Omega_tg_ref); hold on
+    title('Torque Reference')
+    figure(3)
+    myplot(Omega_bg_ref); hold on
+    title('BldPitch Reference')
+end
+ 
+figure
+myplot(Omega_tg_ref); hold on
+myplot(Omega_bg_ref);
+myplot(simout.Time, simout.GenSpeed*pi/30);
+leg = legend('Torque Controller Reference','Blade Pitch Controller Reference', 'Generator Speed');
+leg.Location = 'SouthEast';
+xlabel('Time (s)')
+ylabel('Generator Speed (rad/s)')
+title('Setpoint Smoother Shifting Logic')
+set(gca,'FontSize',12)
